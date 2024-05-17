287376880eae0991f41ea27b643d8d591ce49a81
anthony <anthonyburchard@gmail.com>
2024-05-17 22:39:14 +0200

    Modif script + photo html

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function ajouterPhoto() {
-    var divPhotos = document.getElementById("photos");
-    var nouvellePhoto = document.createElement("img");
-    nouvellePhoto.src = "nouvelle_photo.jpg"; // Remplacez "nouvelle_photo.jpg" par le chemin de votre nouvelle photo
-    nouvellePhoto.alt = "Description de la nouvelle photo";
-    divPhotos.appendChild(nouvellePhoto);
-}
-
 document.getElementById('fileInput').addEventListener('change', function(event) {
     const file = event.target.files[0];
     if (file) {
