5e072a3060da737f4583499b637b9f58625c5277
anthony <anthonyburchard@gmail.com>
2024-05-19 09:45:17 +0200

    Modifs diverses

--- scripttasker.php
+++ scripttasker.php
@@ -1,20 +1,20 @@
-//<?php
+<?php
 // Vérifiez si des données sont envoyées par la méthode POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupérez les données envoyées
-    $cle1 = isset($_POST['clé1']) ? $_POST['clé1'] : '';
-    $cle2 = isset($_POST['clé2']) ? $_POST['clé2'] : '';
+    $key1 = isset($_POST['key1']) ? $_POST['key1'] : '';
+    $key2 = isset($_POST['key2']) ? $_POST['key2'] : '';
 
     // Traitez les données comme nécessaire (par exemple, les enregistrer dans une base de données)
-    echo "Données reçues en POST : clé1 = $cle1, clé2 = $cle2";
+    echo "Données reçues en POST : key1 = $key1, key2 = $key2";
 } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Récupérez les données envoyées par la méthode GET
-    $cle1 = isset($_GET['clé1']) ? $_GET['clé1'] : '';
-    $cle2 = isset($_GET['clé2']) ? $_GET['clé2'] : '';
+    $key1 = isset($_GET['key1']) ? $_GET['key1'] : '';
+    $key2 = isset($_GET['key2']) ? $_GET['key2'] : '';
 
     // Traitez les données comme nécessaire
-    echo "Données reçues en GET : clé1 = $cle1, clé2 = $cle2";
+    echo "Données reçues en GET : key1 = $key1, key2 = $key2";
 } else {
     echo "Aucune donnée reçue.";
 }
-?>//
+?>
