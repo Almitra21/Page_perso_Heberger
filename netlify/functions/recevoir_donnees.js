8d9aad0ab794bfadc441c4997cbaac613fb45ba6
anthony <anthonyburchard@gmail.com>
2024-05-20 21:13:16 +0200

    Modif html et script tasker

--- netlify/functions/recevoir_donnees.js
+++ netlify/functions/recevoir_donnees.js
@@ -1,9 +1,13 @@
 let storedData = [];
 
 exports.handler = async (event, context) => {
+  console.log('Event:', event);
+  console.log('Stored Data:', storedData);
+
   if (event.httpMethod === "POST") {
     const data = JSON.parse(event.body);
     storedData.push(data);
+    console.log('Data after push:', storedData);
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Données reçues", data, storedData }),
@@ -20,3 +24,4 @@ exports.handler = async (event, context) => {
     };
   }
 };
+
