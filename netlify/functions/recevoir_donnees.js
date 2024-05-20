0a9e1de50b0ff790b4a81c23540cd39492575533
anthony <anthonyburchard@gmail.com>
2024-05-20 18:22:09 +0200

    Modif script tasker

--- netlify/functions/recevoir_donnees.js
+++ netlify/functions/recevoir_donnees.js
@@ -1,16 +1,22 @@
+let storedData = [];
+
 exports.handler = async (event, context) => {
-    if (event.httpMethod !== "POST") {
-      return {
-        statusCode: 405,
-        body: "Method Not Allowed",
-      };
-    }
-  
+  if (event.httpMethod === "POST") {
     const data = JSON.parse(event.body);
-  
+    storedData.push(data);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Données reçues", data }),
+      body: JSON.stringify({ message: "Données reçues", data, storedData }),
     };
-  };
-  
+  } else if (event.httpMethod === "GET") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ storedData }),
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+    };
+  }
+};
